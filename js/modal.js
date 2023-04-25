0a867b352c3dceacfc56efa3f226f7dae9af3820
Vitalii1991 <ironwarrior1991@gmail.com>
2023-04-25 19:59:45 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,24 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelectorAll("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
+  const refs = {
+    openModalBtn: document.querySelectorAll("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+    body: document.body,
+  };
 
-    for (let button of refs.openModalBtn) {
-      button.addEventListener("click", toggleModal);
+  for (let button of refs.openModalBtn) {
+    button.addEventListener("click", toggleModal);
+  }
+
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.modal.addEventListener("click", (event) => {
+    if (event.currentTarget === event.target) {
+      toggleModal();
     }
-  
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      refs.body.classList.toggle("no-scroll");
-    }
-  })();
+  });
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
